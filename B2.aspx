2723fbda43ec61c894c236eb37c7bda68c1c40e8
shawki <shawki2207112@stud.kuet.ac.bd>
2026-07-17 23:08:48 +0600

    Add bank account web application

---
+++ B2.aspx
@@ -0,0 +1,25 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="B2.aspx.cs" Inherits="WebApplication1.B2" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <div>
+            <h2>Bank Account Form</h2>
+            <p>Account:
+                <asp:TextBox ID="balance" runat="server" TextMode="Number"></asp:TextBox>
+                <asp:Button ID="deposit" runat="server" OnClick="deposit_Click" Text="Deposite" />
+                <asp:Button ID="withdraw" runat="server" OnClick="withdraw_Click" Text="Withdraw" />
+            </p>
+            <p>
+                <asp:Label ID="lblmsg" runat="server"></asp:Label>
+            </p>
+        </div>
+        <asp:Label ID="lblres" runat="server" ForeColor="Red"></asp:Label>
+    </form>
+</body>
+</html>
---
+++ B2.aspx.cs
@@ -0,0 +1,99 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+
+
+class BankAccount
+{
+    public int accountNumber {  get; set; }
+    public int balance { get; set; }
+
+    public BankAccount(int acc,int b)
+    {  accountNumber = acc; balance = b; }
+
+    public bool deposit(int c)
+    {
+        if (c > 1000)
+        {
+            return false;
+        }
+        else
+        {
+            balance += c;
+            return true; ;
+        }
+    }
+
+    public bool withdraw(int c)
+    {
+        if (balance - c < 0)
+        {
+            return false;
+        }
+        else
+        {
+            balance -= c;
+            return true;
+        }
+    }
+};
+namespace WebApplication1
+{
+    public partial class B2 : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if(!IsPostBack)
+            {
+                Session["bank"] = new BankAccount(1, 0);
+            }
+            showBalance();
+
+        }
+
+        void showBalance()
+        {
+            BankAccount ba = (BankAccount)Session["bank"];
+            lblmsg.Text = "Current balance: " + ba.balance; 
+        }
+
+        protected void withdraw_Click(object sender, EventArgs e)
+        {
+            BankAccount ba = (BankAccount)Session["bank"];
+            int c = Convert.ToInt32(balance.Text);
+
+            if(ba.withdraw(c))
+            {
+                lblres.Text = "Withdraw Successful";
+            }
+            else
+            {
+                lblres.Text = "Invalid Withdraw ";
+            }
+
+            showBalance();
+
+        }
+
+        protected void deposit_Click(object sender, EventArgs e)
+        {
+            BankAccount ba = (BankAccount)Session["bank"];
+            int c = Convert.ToInt32(balance.Text);
+
+            if (ba.deposit(c))
+            {
+                lblres.Text = "Deposit Successful";
+            }
+            else
+            {
+                lblres.Text = "Invalid Deposit";
+               
+            }
+            showBalance();
+        }
+    }
+}
